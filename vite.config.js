ced9c1ebd5e320e8257b2abc8be052cb968c85c1
Olebraende <olembrande@gmail.com>
2025-01-31 10:48:29 +0100

    update base path in Vite configuration to match repository name

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/react-router/", // Viktig: Bruk repo-navnet ditt her!
+  base: "/react-router-project/", // Bytt ut med ditt repo-navn!
 });
