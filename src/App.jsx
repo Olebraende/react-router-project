90e53898cbc3f52a2333b77e61381a0443b2d310
Olebraende <olembrande@gmail.com>
2025-01-31 11:04:57 +0100

    refactor: simplify App component by directly rendering Projects

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,7 @@ function App() {
 
   return (
     <div>
-      <Navbar />
-      <Routes>
-        <Route path='/projects' element={<Projects />} />
-        {/* <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/socials' element={<Socials />} /> */}
-        {/* <Route path='/projects' element={<Projects />} /> */}
-        </Routes>
+        <Projects />
     </div>
   )
 }
